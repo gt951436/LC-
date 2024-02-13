e62097916e66f2a211186baf12c9c36dbb094c79
Garv Tyagi <128500987+GT95codes@users.noreply.github.com>
2024-02-13 14:16:27 +0530

    Time: 53 ms (33.67%) | Memory: 26.5 MB (44.08%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -9,7 +9,7 @@ public:
         return "";
     }
     bool isPalindrome(string s) {
-        int i = 0,j = s.length()-1;
+        int i = 0, j = s.length() - 1;
         while (i < j) {
             if (s[i] != s[j])
                 return false;
