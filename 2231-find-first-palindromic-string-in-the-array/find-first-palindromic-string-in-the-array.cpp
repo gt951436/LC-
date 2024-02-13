a00db0da5118bee9e6e4fd914e8840566c140cdc
Garv Tyagi <128500987+GT95codes@users.noreply.github.com>
2024-02-13 14:14:08 +0530

    Time: 50 ms (50.00%) | Memory: 26.7 MB (16.60%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -10,11 +10,15 @@ public:
     }
     bool isPalindrome(string s){
         int n = s.length();
-        for(int i=0;i < n/2;++i){
-            if(s[i]!=s[n-1-i]){
-                return false;
-            }
-        }
+       int i=0;
+       int j=n-1;
+       while(i<j){
+           if(s[i]!=s[j]){
+              return false;
+           }
+           ++i;
+           --j;
+       }
         return true;
     }
 };
