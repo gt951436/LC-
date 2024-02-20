109fe7b842e001dd3a324e9c755c0ef0e54c2f4f
Garv Tyagi <128500987+GT95codes@users.noreply.github.com>
2024-02-20 12:50:44 +0530

    Time: 11 ms (85.38%) | Memory: 20.5 MB (16.85%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<nums.size();++i){
          sum+=nums[i];
         }
-        long long int S = (n*1LL*(n+1)*1LL)/2;
+        long long int S = ((n*(n+1))/2);
         return S-sum;
     }
 };
