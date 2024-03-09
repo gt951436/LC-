dea685499a021ea8a10b6aeeaacc9df631751d59
Garv Tyagi <128500987+GT95codes@users.noreply.github.com>
2024-03-09 12:09:58 +0530

    Time: 59 ms (93.71%) | Memory: 52.9 MB (73.96%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
-        int i = 0, j = 0;
-        while (i < n1 && j < n2) {
+        for(int i=0,j=0;i < n1 && j < n2;) {
             int x = nums1[i], y = nums2[j];
             if (x == y)
                 return x;
