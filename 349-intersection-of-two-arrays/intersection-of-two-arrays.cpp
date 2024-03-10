857e50eb8bfea4cb205c8b15af397280313ecb76
Garv Tyagi <128500987+GT95codes@users.noreply.github.com>
2024-03-10 15:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (61.63%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
         vector<int> ans;
         bitset<1001> bs1, bs2;
         for (int it : nums1) {
