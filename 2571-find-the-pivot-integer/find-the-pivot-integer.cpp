132a40dac39fea3f4f932347ead92c7ae0be1521
Garv Tyagi <128500987+GT95codes@users.noreply.github.com>
2024-03-13 16:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        long long  total = n * (n + 1) / 2;
+        int total = n * (n + 1) / 2;
         if (PS(total))
             return sqrt(total);
         return -1;
     }
-    bool PS(long long x) {
-        if (x >= 0) {
-
-            long long sr = sqrt(x);
-            return (sr * sr == x);
-        }
+    bool PS(int x) {
+        int sr = sqrt(x);
+        return (sr * sr == x);
         return false;
     }
 };
