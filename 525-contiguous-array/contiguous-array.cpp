62a3bb869b226e64c171d4b5d4ecc5b5a5c954ff
Garv Tyagi <128500987+GT95codes@users.noreply.github.com>
2024-03-16 21:23:08 +0530

    Time: 94 ms (36.36%) | Memory: 87.7 MB (74.78%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -6,7 +6,10 @@ public:
         int sum = 0;
         int len = 0;
         for (int i = 0; i < n; i++) {
-            sum += (nums[i] == 0 ? -1 : 1); 
+            if (nums[i] == 0)
+                --sum;
+            else
+                ++sum;
             if (sum == 0) {
                 len = i + 1;
             } else if (mp.find(sum) != mp.end()) {
