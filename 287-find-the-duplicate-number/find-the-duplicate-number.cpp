6ed56e09d7e375d0aa5b957ca396152b6c2154f6
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-03-24 17:30:17 +0530

    Time: 71 ms (84.67%) | Memory: 63.7 MB (56.88%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         // HARE AND TORTOISE ALGORITHM
-        int s = nums[0]; //SLOW
-        int f = nums[0]; //FAST
+        int s = nums[0], f = nums[0]; // SLOW , FAST
 
         s = nums[s];
         f = nums[nums[f]];
 
-        while (s != f) { //cycle detection
+        while (s != f) { // cycle detection
             s = nums[s];
             f = nums[nums[f]];
-        } 
-        f = nums[0]; 
+        }
+        f = nums[0];
 
-        while(f!=s){  // l1+l2 = n*k ===> l1 = n*k - l2
-            s=nums[s];
+        while (f != s) { // l1+l2 = n*k ===> l1 = n*k - l2
+            s = nums[s];
             f = nums[f];
         }
         return f; // or slow
