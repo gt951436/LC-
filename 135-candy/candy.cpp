cacafa9d8c9301d293147b5cc44a4d227c8671a4
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-03-24 23:21:25 +0530

    Time: 12 ms (80.80%) | Memory: 19.9 MB (83.10%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> candies(n, 1);
+        vector<int> candies(ratings.size(), 1);
         for (int i = 1; i < ratings.size(); ++i) {
             if (ratings[i] > ratings[i - 1])
                 if (candies[i] <= candies[i - 1])
