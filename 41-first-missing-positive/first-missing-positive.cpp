43389466fdd520387758420ecf5d78a188447196
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-03-26 16:40:14 +0530

    Time: 46 ms (47.85%) | Memory: 43.6 MB (81.44%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -11,26 +11,26 @@ public:
                 break;
             }
         }
-        if (isOne) {
-            for (int i = 0; i < n; ++i) {
-                if (nums[i] <= 0 || nums[i] > n) {
-                    nums[i] = 1;
-                }
+        if (!isOne)
+            return 1; // if 1 is not present
+        
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = 1;
             }
-            for (int i = 0; i < n; ++i) {
-                int num = abs(nums[i]);
-                int idx = num - 1;
-                if (nums[idx] < 0)
-                    continue;
-                else
-                    nums[idx] *= -1;
-            }
-            for (int i = 0; i < n; ++i) {
-                if (nums[i] > 0)
-                    return (i + 1);
-            }
-            return (n+1); // all [1,n] are present
         }
-        return 1; // if 1 is not present
+        for (int i = 0; i < n; ++i) {
+            int num = abs(nums[i]);
+            int idx = num - 1;
+            if (nums[idx] < 0)
+                continue;
+            else
+                nums[idx] *= -1;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0)
+                return (i + 1);
+        }
+        return (n + 1); // all [1,n] are present
     }
 };
