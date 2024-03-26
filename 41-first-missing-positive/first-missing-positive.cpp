9197489caea8b438c3fd9b723361da648ceb91a3
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-03-26 16:39:00 +0530

    Time: 46 ms (47.85%) | Memory: 43.6 MB (81.44%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        map<int, int> mp;
-        for (int i = 0; i < nums.size(); ++i) {
-            mp[nums[i]]++;
-        }
-        int i = 1;
-        while (1) {
-            if (mp.find(i) == mp.end()) {
-                return i;
+        int isOne = 0;
+        int n = nums.size();
+
+        // check if 1 is present or not
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 1) {
+                isOne = 1;
+                break;
             }
-            i++;
         }
+        if (isOne) {
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= 0 || nums[i] > n) {
+                    nums[i] = 1;
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                int num = abs(nums[i]);
+                int idx = num - 1;
+                if (nums[idx] < 0)
+                    continue;
+                else
+                    nums[idx] *= -1;
+            }
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] > 0)
+                    return (i + 1);
+            }
+            return (n+1); // all [1,n] are present
+        }
+        return 1; // if 1 is not present
     }
 };
