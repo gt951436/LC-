dba247e4e200df0281c6059564c2df9b6bec470d
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-03-28 19:58:47 +0530

    Time: 196 ms (87.55%) | Memory: 148.4 MB (57.21%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -8,16 +8,13 @@ public:
         int s = 0;
         int e = 0;
         while (e < n) {
-            if (mp[nums[e]] <= k) {
-                mp[nums[e]]++;
-            }
-            while (e < n && mp[nums[e]] > k) {
-
+            mp[nums[e]]++;
+            while(s<e && mp[nums[e]]>k){
                 mp[nums[s]]--;
-                s++;
+                ++s;
             }
-            max_len = max(max_len, e - s + 1);
-            e++;
+            max_len = max(max_len,e-s+1);
+            ++e;
         }
         return max_len;
     }
