7db77c1949a96284ef4c4f8ce4345108accf83cf
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-04-06 11:44:42 +0530

    Time: 19 ms (75.33%) | Memory: 12.3 MB (74.21%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int openP = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(') {
+                openP++;
+            } else if (s[i] == ')') {
+                if (openP == 0)
+                    s[i] = '#';
+                else
+                    openP--;
+            }
+        }
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (openP > 0 && s[i] == '(') {
+                s[i] = '#';
+                --openP;
+            }
+        }
+        string ans="";
+        for(int i=0;i<s.length();++i){
+            if(s[i]!='#')
+            ans+=s[i];
+        }
+        return ans;
+    }
+};
