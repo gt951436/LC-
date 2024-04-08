4d6bc0d1c137e0356aa8166390d230f8effc6edf
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-04-08 10:40:15 +0530

    Time: 5 ms (31.29%) | Memory: 10.7 MB (79.46%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -3,13 +3,13 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int sq = 0;
         int cir = 0;
-        for (int it : students) { // counting the preferences
+        for (auto it : students) { // counting the preferences
             if (it == 1)
                 sq++;
             else
                 cir++;
         }
-        for (int it : sandwiches) {
+        for (auto it : sandwiches) {
             if (it == 0) {
                 if (cir > 0)
                     cir--;
