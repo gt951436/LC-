81c12328414fa27b38d60454443972976c175a5f
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-04-08 10:39:39 +0530

    Time: 5 ms (31.29%) | Memory: 10.8 MB (50.25%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,20 +1,28 @@
-#include <bits/stdc++.h>
 class Solution {
 public:
-    int countStudents(vector<int>& students, vector<int>& sandwiches) { 
-        int remain = sandwiches.size();
-        vector<int> prefer(2);
-        for (int x : students) {
-            prefer[x]++;
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int sq = 0;
+        int cir = 0;
+        for (int it : students) { // counting the preferences
+            if (it == 1)
+                sq++;
+            else
+                cir++;
         }
-        for (int y : sandwiches) {
-            if (prefer[y] == 0)
-                break;
-            if (remain == 0)
-                break;
-            --prefer[y];
-            --remain;
+        for (int it : sandwiches) {
+            if (it == 0) {
+                if (cir > 0)
+                    cir--;
+                else
+                    break;
+            }
+            if (it == 1) {
+                if (sq > 0)
+                    sq--;
+                else
+                    break;
+            }
         }
-        return remain;
+        return sq + cir;
     }
 };
