4c71a5bfe62b2ce8474dbe0807286008f10a8aca
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-05-31 22:31:57 +0530

    Time: 7 ms (62.82%) | Memory: 12.4 MB (64.77%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int>ans;
-        unordered_map<int,int>mp;
-        for(int it:nums){
-            mp[it]++;
+        int xor_total = 0;
+        for (int x : nums) {
+            xor_total ^= x;
         }
-        for(auto it:mp){
-            if(it.second==1){
-              ans.push_back(it.first);
+        unsigned int ryt_set_bit = xor_total & -(unsigned int)xor_total;
+        vector<int>ans(2,0);
+        for(int it:nums){
+            if((ryt_set_bit & it)==0){
+                ans[0]^=it;
+            }
+            else{
+                ans[1]^=it;
             }
         }
         return ans;
