47d679f1f5117e98dc941f0af2212924c40dd829
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-06-10 12:18:53 +0530

    Time: 3 ms (35.74%) | Memory: 10.9 MB (10.73%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> expected = heights;
-        sort(expected.begin(), expected.end());
+        vector<int> sorted = heights;
+        cnt_sort(sorted);
         int gt = 0;
-        for (int i = 0; i < heights.size(); ++i) {
-            if (expected[i]^heights[i])
-                gt++;
-            else
-                continue;
+        for (int i = 0; i < sorted.size(); ++i) {
+            if (sorted[i] != heights[i])
+                ++gt;
         }
         return gt;
+    }
+    void cnt_sort(vector<int>& heights) {
+        map<int, int> mp;
+        int mini = *min_element(heights.begin(), heights.end());
+        int maxi = *max_element(heights.begin(), heights.end());
+        for (int i = 0; i < heights.size(); ++i) {
+            mp[heights[i]]++;
+        }
+        int idx = 0;
+        for (int i = mini; i <= maxi; ++i) {
+            while (mp[i]--) {
+                heights[idx] = i;
+                idx++;
+            }
+        }
     }
 };
