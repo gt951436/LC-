a3e047c5ef8b4103a22a14945b9d51666eea2645
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-06-18 18:04:16 +0530

    Time: 50 ms (61.34%) | Memory: 39.8 MB (82.86%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -2,22 +2,43 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
                             vector<int>& worker) {
-        vector<pair<int, int>> pd;
-        for (int i = 0; i < worker.size(); ++i) {
-            pd.push_back({difficulty[i], profit[i]});
+        vector<pair<int, int>> pd(difficulty.size());
+        for (int i = 0; i < difficulty.size(); ++i) {
+            pd[i] = {difficulty[i], profit[i]};
         }
+        auto comp = [&](pair<int, int> a, pair<int, int> b) {
+            if (a.first < b.first)
+                return true;
+            return false;
+        };
         sort(worker.begin(), worker.end());
-        sort(pd.begin(), pd.end());
-        int i = 0;
+        sort(pd.begin(), pd.end(), comp);
+        int cov = 0;
         int ans = 0;
         int maxi = 0;
-        for (int it : worker) {
-            while (it >= pd[i].first && i < worker.size()) {
+        for(int i=0; i<worker.size(); i++){
+            int idx = bs(pd, worker[i]);
+            for(int i=cov; i<=idx; i++){
                 maxi = max(maxi, pd[i].second);
-                i++;
             }
+            cov = max(0,idx);
             ans += maxi;
         }
         return ans;
+    }
+    int bs(vector<pair<int, int>>& pd, int val) {
+        int l = 0;
+        int r = pd.size() - 1;
+        int res = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (pd[mid].first <= val) {
+                res = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
     }
 };
