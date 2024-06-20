4f1fb4ec1466a8faabd1903a65d6f349e1810e9e
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-06-20 17:26:30 +0530

    Time: 99 ms (93.43%) | Memory: 68.8 MB (92.98%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        if (1LL*m * 1LL*k > bloomDay.size()) {
-            return -1;
-        }
-        long long l = 1;
-        long long r = *max_element(bloomDay.begin(), bloomDay.end());
-        int ans;
-        while (l <= r) {
-            long long mid = l + (r - l) / 2;
-            int bq = 0, fl_cnt = 0;
-            for (int i = 0; i < bloomDay.size(); ++i) {
-                if (bloomDay[i] <= mid) {
-                    fl_cnt++;
-                    if (fl_cnt == k) {
-                        bq++;
-                        fl_cnt = 0;
-                    }
-                } else {
-                    fl_cnt = 0;
-                }
-            }
-            if (bq >= m) {
-                ans = mid;
-                r = mid - 1;
+        int n = bloomDay.size();
+        int start_day = 1;
+        int end_day = *max_element(bloomDay.begin(), bloomDay.end());
+        int minDays = -1;
+        while (start_day <= end_day) {
+            int mid_day = start_day + (end_day - start_day) / 2;
+            if (canMakeBqt(bloomDay, mid_day, k)>=m) {
+                minDays = mid_day;
+                end_day = mid_day - 1;
             } else {
-                l = mid + 1;
+                start_day = mid_day + 1;
             }
         }
-        return ans;
+        return minDays;
+    }
+    int canMakeBqt(vector<int>& bloomDay, int day, int k) {
+        int bq_cnt = 0;
+        int fl_cnt = 0; // k flower chahiye tabhi bouqet bnega
+        for (int i = 0; i < bloomDay.size(); ++i) {
+            if (bloomDay[i] <= day) {
+                fl_cnt++;
+            } else {
+                fl_cnt = 0;
+            }
+            if (fl_cnt == k) {
+                fl_cnt = 0; // reset krdiya
+                bq_cnt++;
+            }
+        }
+        return bq_cnt;
     }
 };
