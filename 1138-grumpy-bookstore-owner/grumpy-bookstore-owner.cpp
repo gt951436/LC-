92c3a0fed065614cffb3367404664481bf23aaa9
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-06-21 18:39:08 +0530

    Time: 366 ms (5.01%) | Memory: 34.3 MB (47.97%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int ans = 0, total = 0;
-        for(int i = 0; i < customers.size(); i++) {
-            total += (1 - grumpy[i]) * customers[i];
+        int n = customers.size();
+        int maxSat = 0, initialTotalSat = 0;
+        for (int i = 0; i < n; ++i) {
+            initialTotalSat += (1 - grumpy[i]) * customers[i];
         }
-        // in the window
-        int all = 0, partial = 0;
-        for(int i = 0; i < customers.size(); i++) {
-            all += customers[i];
-            partial += ((1 - grumpy[i]) * customers[i]);
-            if(i + 1 >= minutes) {
-                ans = max(ans, total - partial + all);
-                int left = i - minutes + 1;
-                all -= customers[left];
-                partial -= ((1 - grumpy[left]) * customers[left]);
+        int maxExtra = 0;
+        for (int i = 0; i < n - minutes + 1; ++i) {
+            int currWinUnSat = 0; // unsatisfied customers
+            for (int j = i; j < i + minutes && j < n; ++j) {
+                if (grumpy[j])
+                    currWinUnSat += customers[j];
             }
+            maxExtra = max(maxExtra, currWinUnSat);
         }
-        return ans;
+        return initialTotalSat + maxExtra;
     }
 };
