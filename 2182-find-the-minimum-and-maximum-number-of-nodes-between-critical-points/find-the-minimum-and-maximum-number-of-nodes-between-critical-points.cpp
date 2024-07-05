8532fb977afe6ca8dc41f6c6ace54a7cfc988a66
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-07-05 21:25:30 +0530

    Time: 172 ms (39.47%) | Memory: 129.3 MB (5.36%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -16,20 +16,26 @@ public:
             nodeValues.push_back(head->val);
             head = head->next;
         }
+        // 3 se kam nodes hone par critical points nahi honge!
         if (nodeValues.size() < 3) {
             return {-1, -1};
         }
         vector<int> idx;
+        // indexes of critical points store karao in idx.
         for (int i = 1; i < nodeValues.size() - 1; ++i) {
             if ((nodeValues[i] > nodeValues[i - 1] && nodeValues[i] > nodeValues[i + 1]) ||
                 (nodeValues[i] < nodeValues[i - 1] && nodeValues[i] < nodeValues[i + 1])) {
                 idx.push_back(i);
             }
         }
+        // agar critical points 2 se kam hue..yaani 1 ya 0..then
+        //return [-1,-1].
         if (idx.size() < 2) {
             return {-1, -1};
         }
         int minDist = INT_MAX;
+        // maxDist between any two critical points to 
+        //last wale index and first wale index ka distance rahega.
         int maxDist = idx.back() - idx.front();
         for (int i = 1; i < idx.size(); ++i) {
             minDist = min(minDist, idx[i] - idx[i - 1]);
