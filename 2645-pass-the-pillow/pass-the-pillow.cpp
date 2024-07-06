e6d7864e8e4f6eee3863a3d20477e7f46280ebcc
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-07-06 15:08:30 +0530

    Time: 2 ms (54.14%) | Memory: 7.1 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -3,9 +3,9 @@ public:
     int passThePillow(int n, int time) {
         int cycle = 2 * (n - 1);
         int effectiveTime = time % cycle;
-        if (effectiveTime < n) {
+        if (effectiveTime <= n - 1) {
             return effectiveTime + 1;
         }
-        return n-(effectiveTime-(n-1));
+        return (n - 1) + (n - effectiveTime);
     }
 };
