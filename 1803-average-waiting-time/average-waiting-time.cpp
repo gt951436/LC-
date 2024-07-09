8839e15381a611f6b20c8baf2b4d955bbd1e39c5
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-07-09 12:29:43 +0530

    Time: 155 ms (46.76%) | Memory: 76.6 MB (57.65%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -2,24 +2,15 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         int n = customers.size();
-        vector<int> CT(n);
+        double totWT = 0.0; // int totWT = 0; // (int) is not working!
+        int ct = 0;
         for (int i = 0; i < n; ++i) {
-            if (i == 0)
-                CT[i] = customers[i][0] + customers[i][1];
-            else {
-                if (CT[i - 1] < customers[i][0])
-                    // AT[i] > CT[i-1]
-                    CT[i] = customers[i][0] + customers[i][1];
-                else
-                    CT[i] = CT[i - 1] + customers[i][1];
-            }
-        }
-        // int totWT = 0; // (int) is not working!
-        double totWT = 0.0;
-        for (int i = 0; i < n; ++i) {
-            totWT += (CT[i] - customers[i][0]);
+            ct = max(ct, customers[i][0]) + customers[i][1];
+            totWT += (ct - customers[i][0]);
         }
         totWT /= n;
         return totWT;
     }
+    //  O(N)-->TC
+    // O(1)-->SC
 };
