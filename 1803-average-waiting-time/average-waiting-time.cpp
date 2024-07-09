d143b4d616254f1189f2703363ab3d33754b8577
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-07-09 11:58:13 +0530

    Time: 142 ms (79.41%) | Memory: 77.6 MB (20.00%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -8,19 +8,18 @@ public:
                 CT[i] = customers[i][0] + customers[i][1];
             else {
                 if (CT[i - 1] < customers[i][0])
-                // AT[i] > CT[i-1]
+                    // AT[i] > CT[i-1]
                     CT[i] = customers[i][0] + customers[i][1];
                 else
                     CT[i] = CT[i - 1] + customers[i][1];
             }
         }
-        double avgWT = 0;
         // int totWT = 0; // (int) is not working!
-        double totWT=0.0;
+        double totWT = 0.0;
         for (int i = 0; i < n; ++i) {
             totWT += (CT[i] - customers[i][0]);
         }
-        avgWT = totWT / n;
-        return avgWT;
+        totWT /= n;
+        return totWT;
     }
 };
