b1afef1efc20bf10decb2003c8a4d9e41222deff
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-07-09 11:56:18 +0530

    Time: 139 ms (85.88%) | Memory: 77.7 MB (20.00%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        int n = customers.size();
+        vector<int> CT(n);
+        for (int i = 0; i < n; ++i) {
+            if (i == 0)
+                CT[i] = customers[i][0] + customers[i][1];
+            else {
+                if (CT[i - 1] < customers[i][0])
+                    CT[i] = customers[i][0] + customers[i][1];
+                else
+                    CT[i] = CT[i - 1] + customers[i][1];
+            }
+        }
+        double avgWT = 0;
+        // int totWT = 0; // (int) is not working!
+        double totWT=0.0;
+        for (int i = 0; i < n; ++i) {
+            totWT += (CT[i] - customers[i][0]);
+        }
+        avgWT = totWT / n;
+        return avgWT;
+    }
+};
