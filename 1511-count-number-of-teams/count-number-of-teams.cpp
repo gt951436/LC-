feb526b6a932905f091e1be54e0ca37b0d739cc8
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-07-29 22:43:15 +0530

    Time: 47 ms (49.84%) | Memory: 11.5 MB (59.43%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -2,17 +2,52 @@ class Solution {
 public:
     int numTeams(vector<int>& rating) {
         int n = rating.size();
-        int cnt = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                for (int k = j + 1; k < n; ++k) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||
-                        (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        ++cnt;
-                    }
-                }
+        int ans = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            ans += countLargerLeft(i, rating) * countSmallerRight(i, rating);
+            ans += countSmallerLeft(i, rating) * countLargerRight(i, rating);
+        }
+        return ans;
+    }
+
+private:
+    int countSmallerLeft(int idx, vector<int>& rating) {
+        int count = 0;
+        for (int i = 0; i < idx; ++i) {
+            if (rating[i] < rating[idx]) {
+                count++;
             }
         }
-        return cnt;
+        return count;
+    }
+
+    int countLargerLeft(int idx, vector<int>& rating) {
+        int count = 0;
+        for (int i = 0; i < idx; ++i) {
+            if (rating[i] > rating[idx]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int countSmallerRight(int idx, vector<int>& rating) {
+        int count = 0;
+        for (int i = idx + 1; i < rating.size(); ++i) {
+            if (rating[i] < rating[idx]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int countLargerRight(int idx, vector<int>& rating) {
+        int count = 0;
+        for (int i = idx + 1; i < rating.size(); ++i) {
+            if (rating[i] > rating[idx]) {
+                count++;
+            }
+        }
+        return count;
     }
 };
